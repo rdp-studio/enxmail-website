b56acf8218dd6782e39382583c555900cad8388c
rdp-studio <67402215+rdp-studio@users.noreply.github.com>
2022-11-23 10:47:45 +0000

    Deploying to gh-pages from @ rdp-studio/enxmail-website@fa920c78e7b4ffb223ec15a6021ff150f778822d 🚀

--- service.js
+++ service.js
@@ -8,70 +8,75 @@ function obj(root, parts) {
     return obj(root[root_name], parts)
 }
 
-$(function () {
-    if (window.location.pathname !== "/profile/") {
-        return;
-    }
-
-    var fingerprint = window.location.search.substr(1);
-    if( fingerprint[0] === '!' )
-        fingerprint = fingerprint.substr(1);
-
-    if (fingerprint.length !== 37) {
-        window.location.href = "/guide/#profile-page-guide";
-        return;
-    }
-
-    var url = "https://enxmail_api.rdpstudio.top/api/v1/keyserver/get?fingerprint=" + fingerprint;
-    $.getJSON(url, function (data) {
-        document.title = "Profile: " + escape(data.username);
-        console.log(document.title);
-
-        $('*[class^="unit."]').each(function (_, element) {
-            var name = $(element).attr('class').split(' ')[0];
-            var parts = name.split('.');
-            console.log(name);
-            console.log(parts);
-
-            // remove unit.
-            parts.shift();
-            // console.log(parts);
-            var v = obj(data, parts);
-            // console.log("v = " + v);
-
-            if (true) {
-                finger_test = name.indexOf("_fingerprint");
-                console.log(finger_test);
-                if (finger_test !== -1){
-                    $(element).text(fingerprint);
-                } else if(v !== null) {
+setTimeout(function(){
+    $(function () {
+        if (window.location.pathname !== "/profile/") {
+            return;
+        }
+    
+        var fingerprint = window.location.search.substr(1);
+        if( fingerprint[0] === '!' )
+            fingerprint = fingerprint.substr(1);
+    
+        if (fingerprint.length !== 37) {
+            window.location.href = "/guide/#profile-page-guide";
+            return;
+        }
+    
+        var url = "https://enxmail_api.rdpstudio.top/api/v1/keyserver/get?fingerprint=" + fingerprint;
+        $.getJSON(url, function (data) {
+            if (data["code"] == 1){
+                window.location.href = "/";
+            }
+            document.title = "Profile: " + escape(data.username);
+            console.log(document.title);
+    
+            $('*[class^="unit."]').each(function (_, element) {
+                var name = $(element).attr('class').split(' ')[0];
+                var parts = name.split('.');
+                console.log(name);
+                console.log(parts);
+    
+                // remove unit.
+                parts.shift();
+                // console.log(parts);
+                var v = obj(data, parts);
+                // console.log("v = " + v);
+    
+                if (true) {
+                    finger_test = name.indexOf("_fingerprint");
+                    console.log(finger_test);
+                    if (finger_test !== -1){
+                        $(element).text(fingerprint);
+                    } else if(v !== null) {
+                        $(element).text(v);
+                    }
+                }
+            });
+        });
+    });
+    
+    $(function () {
+        if (window.location.pathname !== "/") {
+            return;
+        }
+    
+        var url = "https://enxmail_api.rdpstudio.top/api/v1/clientserver/sysstatus";
+        $.getJSON(url, function (data) {
+            $('*[class^="sys."]').each(function (_, element) {
+                var name = $(element).attr('class').split(' ')[0];
+                var parts = name.split('.');
+    
+                // remove unit.
+                parts.shift();
+                // console.log(parts);
+                var v = obj(data, parts);
+                // console.log("v = " + v);
+    
+                if (v !== null) {
                     $(element).text(v);
                 }
-            }
+            });
         });
     });
-});
-
-$(function () {
-    if (window.location.pathname !== "/") {
-        return;
-    }
-
-    var url = "https://enxmail_api.rdpstudio.top/api/v1/clientserver/sysstatus";
-    $.getJSON(url, function (data) {
-        $('*[class^="sys."]').each(function (_, element) {
-            var name = $(element).attr('class').split(' ')[0];
-            var parts = name.split('.');
-
-            // remove unit.
-            parts.shift();
-            // console.log(parts);
-            var v = obj(data, parts);
-            // console.log("v = " + v);
-
-            if (v !== null) {
-                $(element).text(v);
-            }
-        });
-    });
-});
+}, 1000);
