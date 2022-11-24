0ddda86e216cc861d926ea485666c26c69e13094
rdp-studio <67402215+rdp-studio@users.noreply.github.com>
2022-11-24 05:47:47 +0000

    Deploying to gh-pages from @ rdp-studio/enxmail-website@f568b59e080255c1409a5d777a8574bffd379ca3 🚀

---
+++ assets/index.html.a63f7733.js
@@ -0,0 +1,15 @@
+import{_ as e}from"./_plugin-vue_export-helper.cdc0426e.js";import{o as i,c as n,a as t}from"./app.722a0720.js";const a={},r=t(`<h2 id="step-by-step-guide" tabindex="-1"><a class="header-anchor" href="#step-by-step-guide" aria-hidden="true">#</a> Step By Step guide</h2><ol><li>First download the EnxMail client. NOTE: It is still under test. You can join QQ group 576738755 to obtain this client.</li><li>Unzip it.</li><li>After decompression, enter the enxmail folder, enter <code>cmd</code> in the title bar of Windows File Explorer, and then enter <code>enxmail -h</code> to start your use.</li><li>If you have any other questions, you can join QQ group 576738755 to ask our team and other users.</li></ol><h2 id="profile-page-guide" tabindex="-1"><a class="header-anchor" href="#profile-page-guide" aria-hidden="true">#</a> Profile page guide</h2><ol><li>Check your fingerprints. Usually in the enxmail folder, the name is fingerprint.txt</li><li>Add your fingerprint after the address <code>https://enxmail.rdpstudio.top/profile/?!</code>.</li><li>This is your profile page.</li></ol><h2 id="profile-introduction-guide" tabindex="-1"><a class="header-anchor" href="#profile-introduction-guide" aria-hidden="true">#</a> Profile introduction guide</h2><p>Simply send an email to the EnxMail address <code>Enx1xsetui</code> (the content is the introduction you want to set) to complete the setting and display it on your profile page.</p><h2 id="command-line-parameter-document" tabindex="-1"><a class="header-anchor" href="#command-line-parameter-document" aria-hidden="true">#</a> Command line parameter document</h2><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>EnxMail
+
+Usage:
+  enxmail mail send &lt;fingerprint&gt; &lt;message&gt; [-v]
+  enxmail mail list [-v]
+  enxmail mail get &lt;mail_id&gt; [-v]
+  enxmail init &lt;username&gt; [-v]
+  enxmail (-h | --help)
+  enxmail --version
+
+Options:
+  -h --help     Show this screen.
+  -v            Enable verbose mode.
+  --version     Show version.
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><h2 id="premium-service" tabindex="-1"><a class="header-anchor" href="#premium-service" aria-hidden="true">#</a> Premium Service</h2><p>If you do not purchase and active premium service, the maximum length of ciphertext will be limited to 768 bytes (about 600 bytes of plaintext, but we limit it to 512 bytes).</p><p>If you want to purchase premium service, join the QQ group 576738755 and contact our team.</p>`,11),d=[r];function l(o,s){return i(),n("div",null,d)}const m=e(a,[["render",l],["__file","index.html.vue"]]);export{m as default};
